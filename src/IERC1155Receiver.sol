9d37e77c6ad43873496cf1deeedc32470e70fefe
Adam Boudj <boudjemaa.adam@gmail.com>
2023-05-05 13:42:43 +0400

    🔧 add solhint config

---
+++ src/IERC1155Receiver.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+interface IERC1155Receiver {
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4);
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external returns (bytes4);
+
+}
